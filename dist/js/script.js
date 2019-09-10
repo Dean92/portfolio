4aae4490124cd7c2b41af87856f9b1cfdd03c053
Dean McCoy <dean92@gmail.com>
2019-09-09 23:56:35 -0500

    added favicon. cleaned up some styling.

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,19 @@
-$(document).ready(function() {
+$(window).on("load", function () {
+    $(".loader .inner").fadeOut(500, function () {
+        $(".loader").fadeOut(750);
+    });
+
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    });
+});
+
+$(document).ready(function () {
     $('#slides').superslides({
         animation: 'fade',
         play: 6000,
@@ -14,32 +29,32 @@ $(document).ready(function() {
     });
 
     $('.owl-carousel').owlCarousel({
-        loop:true,
+        loop: true,
         items: 4,
-        responsive:{
-            0:{
-                items:1
+        responsive: {
+            0: {
+                items: 1
             },
-            480:{
-                items:2
+            480: {
+                items: 2
             },
-            768:{
-                items:3
+            768: {
+                items: 3
             },
-            938:{
-                items:4
+            938: {
+                items: 4
             }
         }
     });
 
-   
+
 
     var skillsTopOffset = $(".skillsSection").offset().top;
     var statsTopOffset = $(".statsSection").offset().top;
     var countUpFinished = false;
-    $(window).scroll(function() {
+    $(window).scroll(function () {
 
-        if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+        if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
             $('.chart').easyPieChart({
                 easing: 'easeInOut',
                 barColor: '#fff',
@@ -47,14 +62,14 @@ $(document).ready(function() {
                 scaleColor: false,
                 lineWidth: 4,
                 size: 152,
-                onStep: function(from, to, percent) {
+                onStep: function (from, to, percent) {
                     $(this.el).find('.percent').text(Math.round(percent));
                 }
             });
         }
 
-        if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
-            $(".count").each(function() {
+        if (!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+            $(".count").each(function () {
                 var element = $(this);
                 var endVal = parseInt(element.text());
                 element.counterUp(endVal);
@@ -66,21 +81,14 @@ $(document).ready(function() {
 
     $("[data-fancybox]").fancybox();
 
-    $(".items").isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 1500,
-            easing: 'linear',
-            queue: false
-        }
-    });
 
-    $("#filters a").click(function() {
+
+    $("#filters a").click(function () {
         $("#filters .current").removeClass("current");
         $(this).addClass("current");
 
         var selector = $(this).attr("data-filter");
-        
+
         $(".items").isotope({
             filter: selector,
             animationOptions: {
@@ -94,12 +102,14 @@ $(document).ready(function() {
 
     });
 
-    $("#navigation li a").click(function(e) {
+    $("#navigation li a").click(function (e) {
         e.preventDefault();
 
         var targetElement = $(this).attr("href");
         var targetPosition = $(targetElement).offset().top;
-        $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow")
+        $("html, body").animate({
+            scrollTop: targetPosition - 50
+        }, "slow")
     })
 
     const nav = $("#navigation");
@@ -108,19 +118,18 @@ $(document).ready(function() {
     $(window).on("scroll", stickyNavigation)
 
     function stickyNavigation() {
-        var body =$("body");
+        var body = $("body");
 
-        if($(window).scrollTop() >= navTop) {
+        if ($(window).scrollTop() >= navTop) {
             body.css("padding-top", nav.outerHeight() + "px");
             body.addClass("fixedNav");
-        } 
-        else {
+        } else {
             body.css("padding-top", 0);
             body.removeClass("fixedNav");
         }
     }
 
-     
+
 
 
 
